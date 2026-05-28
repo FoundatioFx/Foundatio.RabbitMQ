578828fa96d2ae6fa3c2505950ac979b537ba4d7
Blake Niemyjski <bniemyjski@gmail.com>
2026-05-28 11:12:59 -0500

    style: rename s_cache to Cache per project conventions

--- src/Foundatio.RabbitMQ/Extensions/EnumExtensions.cs
+++ src/Foundatio.RabbitMQ/Extensions/EnumExtensions.cs
@@ -7,11 +7,11 @@ namespace Foundatio.Messaging;
 
 internal static class EnumExtensions
 {
-    private static readonly ConcurrentDictionary<Enum, string> s_cache = new();
+    private static readonly ConcurrentDictionary<Enum, string> Cache = new();
 
     public static string ToEnumString<T>(this T value) where T : struct, Enum
     {
-        return s_cache.GetOrAdd(value, static v =>
+        return Cache.GetOrAdd(value, static v =>
         {
             var member = v.GetType().GetField(v.ToString()!);
             var attribute = member?.GetCustomAttribute<EnumMemberAttribute>();
